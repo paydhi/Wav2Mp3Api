fbe53f7ac19a1acd45f6c1d45afef242fea1adb4
Paydhi <paydhi@gmail.com>
2023-05-30 00:09:52 +0600

    Users - done

--- wav2mp3_api/users/models.py
+++ wav2mp3_api/users/models.py
@@ -4,6 +4,6 @@ from django.db import models
 
 
 class User(models.Model):
-    username = models.CharField(max_length=256, unique=True, editable=False)
+    username = models.CharField(max_length=256, unique=True, null=False, blank=False)
     access_token = models.UUIDField(max_length=64, default=uuid.uuid4, editable=False)
     user_uuid = models.UUIDField(max_length=64, default=uuid.uuid4, editable=False)
--- wav2mp3_api/users/views.py
+++ wav2mp3_api/users/views.py
@@ -1,37 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from users.serializers import CreateUserSerializer, ResponseUserSerializer
-from loguru import logger
-from users.models import User
 
 
 class CreateUserView(APIView):
 
     def post(self, request):
         post_user_serializer = CreateUserSerializer(data=request.data)
-
-        if not post_user_serializer.is_valid():
-            return Response(post_user_serializer.errors, status=400)
-
-        if User.objects.filter(username=request.data.get('username')).exists():
-            return Response({'error': 'Username already exists'}, status=400)
-
-        username = request.data.get('username')
-        user = User(username=username)
-        user.save()
-
-        response_data = {'access_token': user.access_token, 'user_uuid': user.user_uuid}
-        logger.info(f'Data for serializer: {response_data}')
-
-        response_serializer = ResponseUserSerializer(data=response_data)
-
-        if response_serializer.is_valid(raise_exception=True):
-            logger.info(f'User {username} created successfully')
-
-            logger.info(f'serializer.data: {response_serializer.data}')
-            logger.info(f'serializer.errors: {response_serializer.errors}')
-            logger.info(f'serializer.validated_data: {response_serializer.validated_data}')
-
-            return Response(response_serializer.validated_data, status=201)
-        else:
-            return Response(response_serializer.errors, status=201)
+        post_user_serializer.is_valid(raise_exception=True)
+        user = post_user_serializer.save()
+        response_serializer = ResponseUserSerializer(instance=user)
+        return Response(response_serializer.data, status=201)
