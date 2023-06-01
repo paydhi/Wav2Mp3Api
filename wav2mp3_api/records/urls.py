1ab5ddd529c7c565b39126aa73b0fa85c4bab52d
Paydhi <paydhi@gmail.com>
2023-06-02 00:26:03 +0600

    /api/records/upload_record/ endpoint returns working download url

--- wav2mp3_api/records/urls.py
+++ wav2mp3_api/records/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from records.views import UploadRecordView#, DownloadRecordView
+from records.views import UploadRecordView, DownloadRecordView
 
 urlpatterns = [
     path('upload_record/', UploadRecordView.as_view(), name='upload_record'),
-    # path('download/', DownloadRecordView.as_view(), name='download_record'),
+    path('download_record/', DownloadRecordView.as_view(), name='download_record'),
 ]
--- wav2mp3_api/records/views.py
+++ wav2mp3_api/records/views.py
@@ -1,5 +1,8 @@
 import os
 
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django.urls import reverse
 from pydub import AudioSegment
 
 from rest_framework.response import Response
@@ -16,18 +19,40 @@ from io import BytesIO
 
 class UploadRecordView(APIView):
     def post(self, request):
-        user = User.objects.get(user_uuid=request.data.get('user_uuid'))
+        if Record.objects.filter(user__user_uuid=request.data.get('user_uuid')).exists():
+            Record.objects.filter(user__user_uuid=request.data.get('user_uuid')).delete()
+            logger.info('Deleted all records for user %s', request.data.get('user_uuid'))
 
-        file = request.FILES['file']
+        file = request.FILES.get('file')
         suffix = f'_{request.data.get("user_uuid")[:4]}.mp3'
         filename_mp3 = file.name.replace(".wav", suffix)
 
         record_mp3 = BytesIO()
-        record = AudioSegment.from_wav(BytesIO(file.read()))
-        record.export(record_mp3, format='mp3')
+        record_wav = AudioSegment.from_wav(BytesIO(file.read()))
+        record_wav.export(record_mp3, format='mp3')
         record_mp3.seek(0)
+
         record_to_save = ContentFile(record_mp3.read(), name=filename_mp3)
+        user = User.objects.get(user_uuid=request.data.get('user_uuid'))
+        record = Record.objects.create(record=record_to_save, user=user)
+        download_url = request.build_absolute_uri(
+           reverse('download_record') + f'?id={record.record_uuid}&user={user.user_uuid}')
 
-        Record.objects.create(record=record_to_save, user=user)
+        response = Response({'download_record': {'download_url': download_url,
+                                                 'id': record.record_uuid,
+                                                 'user': user.user_uuid}})
 
-        return Response({'status': 'success'})
+        return response
+
+
+class DownloadRecordView(APIView):
+    def get(self, request):
+        record_uuid = request.GET.get('id')
+        user_uuid = request.GET.get('user')
+
+        record = get_object_or_404(Record, record_uuid=record_uuid, user__user_uuid=user_uuid)
+
+        response = HttpResponse(record.record, content_type='audio/mpeg')
+        response['Content-Disposition'] = f'attachment; filename={record.record.name}'
+
+        return response
