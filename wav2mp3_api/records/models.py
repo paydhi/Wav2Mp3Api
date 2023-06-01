1c158fe7d5c7496898e7be2e435594cf29dfeb4d
Paydhi <paydhi@gmail.com>
2023-06-01 23:08:31 +0600

    Added delition of file on disk, when deleting object from db

--- wav2mp3_api/records/models.py
+++ wav2mp3_api/records/models.py
@@ -1,6 +1,10 @@
+import os
 import uuid
 
 from django.db import models
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
+
 from users.models import User
 
 
@@ -9,3 +13,9 @@ class Record(models.Model):
     record = models.FileField(upload_to='uploads/')
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+
+@receiver(pre_delete, sender=Record)
+def delete_related_file(sender, instance, **kwargs):
+    if instance.record:
+        if os.path.isfile(instance.record.path):
+            os.remove(instance.record.path)
