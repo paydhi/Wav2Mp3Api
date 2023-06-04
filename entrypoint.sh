44a5e3a9e89e4612944625ab8769375ce1afeb22
Paydhi <paydhi@gmail.com>
2023-06-05 04:37:08 +0600

    Fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,6 @@ set -e  # Exit immediately if a command exits with a non-zero status
 set -x  # all executed commands are printed to the terminal.
 
 
-python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 python manage.py runserver 0.0.0.0:8000
