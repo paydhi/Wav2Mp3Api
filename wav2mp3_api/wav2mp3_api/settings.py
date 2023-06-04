17e56eb04054685cf6796e0b30c5f4dadbc2cc5d
Paydhi <paydhi@gmail.com>
2023-06-05 04:36:07 +0600

    Fix

--- wav2mp3_api/wav2mp3_api/settings.py
+++ wav2mp3_api/wav2mp3_api/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)i9^)!ps)n5h*=b4x)=v%h@#zq(-vb$^_z^c1tlu62e$grv#a4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
